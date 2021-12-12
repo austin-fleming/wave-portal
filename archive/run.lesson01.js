824a632e1e36950f6169d0b406951a3bf9ead61e
flimflam-austin <68439674+flimflam-austin@users.noreply.github.com>
2021-12-11 18:40:29 -0800

    lesson getting total waves

--- archive/run.lesson01.js
+++ archive/run.lesson01.js
@@ -6,29 +6,11 @@
 */
 
 async function main() {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); // compiles contract and adds files to /artifacts
     const waveContract = await waveContractFactory.deploy(); // hardhat spins up local blockchain instance then runs contract
     await waveContract.deployed();
 
     console.log("Contract deployed to:", waveContract.address); // gives address of deployed contract. Important when deploying to actual chain.
-    console.log("Contract deployed by:", owner.address);
-
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves(); // Log shows that this is the 'owner' waving.
-
-    waveTxn = await waveContract.connect(randomPerson).wave(); // Simulates an external person calling wave.
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-    
-    senderHistory = await waveContract.getSenderHistory();
-    console.log('Sender History:\n', senderHistory);
 }
 
 const runMain = async () => {
--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,26 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address[] senderHistory;
+
+
     constructor() {
         console.log("My first contract!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        senderHistory.push(msg.sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getSenderHistory() public view returns (address[] memory) {
+        return senderHistory;
     }
 }
