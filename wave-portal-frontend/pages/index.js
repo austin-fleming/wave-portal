7928cc6ebd8c3b6c9198816a22199cc9f59d7f87
flimflam-austin <68439674+flimflam-austin@users.noreply.github.com>
2021-12-12 15:20:44 -0800

    add connect to wallet functionality

--- wave-portal-frontend/pages/index.js
+++ wave-portal-frontend/pages/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Head from 'next/head'
 import NextImage from 'next/image'
 import S from '../styles/Home.module.css'
@@ -18,7 +18,7 @@ const sampleData = [
   }
 ]
 
-const WaveButton = () => {
+const WaveButton = ({}) => {
   const [buttonWasClicked, setButtonWasClicked] = useState(false);
 
   const clickHandler = (e) => {
@@ -26,6 +26,8 @@ const WaveButton = () => {
     setButtonWasClicked(true)
     console.log('clicked!')
   }
+
+
   return (
     <button
       className={S.hero__waveButton}
@@ -45,9 +47,71 @@ const HistoryRecord = ({waveDate, address}) => (
   </div>
 )
 
-const Home = () => (
+const Home = () => {
+  const [errorMessage, setErrorMessage] = useState("")
+  const [currentAccount, setCurrentAccount] = useState("")
+
+  const checkIfMetaMaskWalletIsConnected = async () => {
+    // If visitor has MetaMask installed, it injects an "ethereum" object into the window
+    // This detects this object
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log("No Ethereum object found in window.");
+        setErrorMessage("Looks like you don't have MetaMask installed.")
+        return
+      } else {
+        console.log("Ethereum object found", ethereum)
+      }
+
+      // Check if we are authorized to access the user's wallet
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      // Just grabbing the first available account. There could be multiple.
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found.")
+        setErrorMessage("Looks like you need to link your MetaMask first.")
+      }
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    checkIfMetaMaskWalletIsConnected();
+  }, [])
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask");
+        return
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+      
+      console.log("Connected", accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
     <>
       <main className={S.main}>
+        {errorMessage && (
+          <section className={S.metamaskBanner}>
+            <p className={S.metamaskBanner__message}>{errorMessage}</p>
+          </section>
+        )}
         <section className={S.hero}>
             <span className={S.hero__hand}>👋</span>
             <h1 className={S.hero__headline}>Give Me a Hand</h1>
@@ -59,7 +123,14 @@ const Home = () => (
                 turned idk anymore 🤷‍♂️
             </h2>
             <p className={S.hero__prompt}>If you&apos;d like to say hi, just hit the button below<br/>👇</p>
-            <WaveButton/>
+            {currentAccount
+              ? (
+                <WaveButton/>
+              )
+              : (
+                <button className={S.hero__waveButton} onClick={connectWallet}>Connect Wallet</button>
+              )
+            }
             <p className={S.hero__buttonSubtext}>it&apos;ll live forever<br/>on the blockchain</p>
         </section>
         <section className={S.history}>
@@ -73,5 +144,5 @@ const Home = () => (
       </main>
     </>
   )
-
+}
 export default Home
